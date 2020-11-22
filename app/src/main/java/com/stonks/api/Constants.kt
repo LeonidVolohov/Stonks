db1154b455cc57c1d621cb71e7da4a0f09ae0128
LeonidVolohov <leka.volohov@gmail.com>
2020-11-22 14:37:43 +0300

    Initial commit

---
+++ app/src/main/java/com/stonks/api/Constants.kt
@@ -0,0 +1,11 @@
+package com.stonks.api
+
+class Constants {
+    companion object {
+        const val STOCK_API_BASE_URL = ""
+
+        const val CURRENCY_API_BASE_URL = ""
+
+        const val CRYPTOCURRENCY_API_BASE_URL = ""
+    }
+}
