b7cbd18c5409ff985d9708072e4b7e37a52e123e
LeonidVolohov <leka.volohov@gmail.com>
2021-01-03 13:49:12 +0300

    Currency-Chart:  - Received currency rates for last month and put its values on a separate list for further plotting

--- app/src/main/java/com/stonks/api/currency/CurrencyPerMonth.kt
+++ app/src/main/java/com/stonks/api/currency/CurrencyPerMonth.kt
@@ -4,5 +4,5 @@ data class CurrencyPerMonth(
         val base: String?,
         val end_at: String?,
         val start_at: String?,
-        val rates: HashMap<String, HashMap<String, Double>>?
+        val rates: HashMap<String, Map<String, Double>>?
 )
--- app/src/main/java/com/stonks/ui/currency/CurrencyMain.kt
+++ app/src/main/java/com/stonks/ui/currency/CurrencyMain.kt
@@ -1,8 +1,11 @@
 package com.stonks.ui.currency
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.stonks.R
 import com.stonks.api.currencyApi
@@ -10,12 +13,17 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import java.lang.Double.parseDouble
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class CurrencyMain : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currency)
+
+        val TAG: String = CurrencyMain::class.java.name
 
         val baseRateSpinner = findViewById<Spinner>(R.id.base_rate_spinner)
         val targetRateSpinner = findViewById<Spinner>(R.id.target_rate_spinner)
@@ -24,8 +32,8 @@ class CurrencyMain : AppCompatActivity() {
         val resultRateTextView = findViewById<TextView>(R.id.rate_result)
         val dateRateTextView = findViewById<TextView>(R.id.rate_data)
         val calculateButton = findViewById<Button>(R.id.calculate_button)
-        lateinit var baseRateSpinnerString : String
-        lateinit var targetRateSpinnerString : String
+        lateinit var baseRateSpinnerString: String
+        lateinit var targetRateSpinnerString: String
         var isNumeric: Boolean
 
         val baseRateAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ratesArray)
@@ -56,8 +64,11 @@ class CurrencyMain : AppCompatActivity() {
 
         loadApiDate(dateRateTextView)
 
+        val currentMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        val previousMonth = LocalDate.now().minusMonths(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+
         calculateButton.setOnClickListener {
-            if(baseRateSpinnerString == targetRateSpinnerString) {
+            if (baseRateSpinnerString == targetRateSpinnerString) {
                 resultRateTextView.text = stringMultiplication(rateNumberEditText.text.toString(), "1.0")
             } else {
                 isNumeric = try {
@@ -67,14 +78,41 @@ class CurrencyMain : AppCompatActivity() {
                     false
                 }
 
-                if(isNumeric) {
+                if (isNumeric) {
                     loadTargetRatePrice(baseRateSpinnerString, targetRateSpinnerString, rateNumberEditText, resultRateTextView)
+
+                    // Get value rates for last month
+                    val rateListPerMonth: MutableList<String> = arrayListOf()
+                    val compositeDisposable = CompositeDisposable()
+                    compositeDisposable.add(
+                            currencyApi.getRatesPerMonth(startDate = previousMonth, endDate = currentMonth,
+                                    targetRate = targetRateSpinnerString, baseRate = baseRateSpinnerString)
+                                    .observeOn(AndroidSchedulers.mainThread())
+                                    .subscribeOn(Schedulers.io())
+                                    .subscribe(
+                                            { response ->
+                                                val dateList: List<String>? = response.rates?.keys?.sortedBy {
+                                                    LocalDate.parse(it, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                                                }
+                                                if (dateList != null) {
+                                                    for (date in dateList) {
+                                                        rateListPerMonth.add(response.rates.get(date)?.get(targetRateSpinnerString).toString())
+                                                    }
+                                                }
+                                                Log.i(TAG, rateListPerMonth.toString())
+                                            },
+                                            { failure ->
+                                                Toast.makeText(this@CurrencyMain, failure.message, Toast.LENGTH_SHORT).show()
+                                            }
+                                    )
+                    )
                 } else {
                     Toast.makeText(this, "Wrong input", Toast.LENGTH_LONG).show()
                 }
             }
         }
     }
+
 
     /**
      * Умножает две строки [firstNumber]  и [secondNumber] и возвращает только первые 7 символов
@@ -87,8 +125,8 @@ class CurrencyMain : AppCompatActivity() {
      * Загружает в [textView] дату, полученную с API
      * */
     private fun loadApiDate(textView : TextView) {
-        val compositeDisposable3 = CompositeDisposable()
-        compositeDisposable3.add(
+        val compositeDisposable = CompositeDisposable()
+        compositeDisposable.add(
                 currencyApi.getRatesPerDay(rate = "EUR")
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribeOn(Schedulers.io())
@@ -105,8 +143,8 @@ class CurrencyMain : AppCompatActivity() {
      * Загружает в [textView] стоимость [baseRate] относительно [targetRate] со множителем [rateNumber]
      */
     private fun loadTargetRatePrice(baseRate : String, targetRate: String, rateNumber: EditText, textView: TextView) {
-        val compositeDisposable2 = CompositeDisposable()
-        compositeDisposable2.add(
+        val compositeDisposable = CompositeDisposable()
+        compositeDisposable.add(
                 currencyApi.getRatesPerDay(rate = baseRate)
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribeOn(Schedulers.io())
