bc102776fdcd49eae6cfbba9d532c4a1726ca8b4
LeonidVolohov <leka.volohov@gmail.com>
2021-02-14 11:30:11 +0300

    Currency-Fragment: - Added stocks files from develop branch

--- app/src/main/java/com/stonks/api/ApiService.kt
+++ app/src/main/java/com/stonks/api/ApiService.kt
@@ -2,10 +2,8 @@ package com.stonks.api
 
 import com.stonks.api.Constants.Companion.CRYPTOCURRENCY_API_BASE_URL
 import com.stonks.api.Constants.Companion.CURRENCY_API_BASE_URL
-import com.stonks.api.Constants.Companion.STOCK_API_BASE_URL
 import com.stonks.api.cryptocurrency.CryptoCurrencyRequests
 import com.stonks.api.currency.CurrencyRequests
-import com.stonks.api.stocks.StocksRequests
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -23,10 +21,3 @@ val cryptoCurrencyApi: CryptoCurrencyRequests = Retrofit.Builder()
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
         .create(CryptoCurrencyRequests::class.java)
-
-val stocksApi = Retrofit.Builder()
-        .baseUrl(STOCK_API_BASE_URL)
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(StocksRequests::class.java)
