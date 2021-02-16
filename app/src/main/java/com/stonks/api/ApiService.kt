5b8db84f928b9a62398c8eabf9b96f86f50a8e0a
kramarov-evg <38880539+kramarov-evg@users.noreply.github.com>
2021-02-16 17:29:30 +0300

    Stocks api implemented (#27)
    * Implemented stocks API interaction
    
    Changes:
    - Defined data model
    - Created API-communicator interface
    - Created functions to retrieve data (w/caching)
    - Added API calls logging
    - Created  custom period API data retrieval
    - Improved disposables behaviour
    - General code clean-up

--- app/src/main/java/com/stonks/api/ApiService.kt
+++ app/src/main/java/com/stonks/api/ApiService.kt
@@ -2,16 +2,28 @@ package com.stonks.api
 
 import com.stonks.api.Constants.Companion.CRYPTOCURRENCY_API_BASE_URL
 import com.stonks.api.Constants.Companion.CURRENCY_API_BASE_URL
+import com.stonks.api.Constants.Companion.STOCK_API_BASE_URL
 import com.stonks.api.cryptocurrency.CryptoCurrencyRequests
 import com.stonks.api.currency.CurrencyRequests
+import com.stonks.api.stocks.StocksRequests
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+
+val interceptor: HttpLoggingInterceptor =
+    HttpLoggingInterceptor().apply { this.level = HttpLoggingInterceptor.Level.BODY }
+
+val client: OkHttpClient = OkHttpClient.Builder()
+    .addInterceptor(interceptor)
+    .build()
 
 val currencyApi: CurrencyRequests = Retrofit.Builder()
     .baseUrl(CURRENCY_API_BASE_URL)
     .addConverterFactory(GsonConverterFactory.create())
     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+    .client(client)
     .build()
     .create(CurrencyRequests::class.java)
 
@@ -19,5 +31,14 @@ val cryptoCurrencyApi: CryptoCurrencyRequests = Retrofit.Builder()
     .baseUrl(CRYPTOCURRENCY_API_BASE_URL)
     .addConverterFactory(GsonConverterFactory.create())
     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+    .client(client)
     .build()
     .create(CryptoCurrencyRequests::class.java)
+
+val stocksApi: StocksRequests = Retrofit.Builder()
+    .baseUrl(STOCK_API_BASE_URL)
+    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+    .addConverterFactory(GsonConverterFactory.create())
+    .client(client)
+    .build()
+    .create(StocksRequests::class.java)
--- app/src/main/java/com/stonks/api/Constants.kt
+++ app/src/main/java/com/stonks/api/Constants.kt
@@ -5,6 +5,7 @@ class Constants {
         const val CURRENCY_API_BASE_URL = "https://api.exchangeratesapi.io/"
 
         const val STOCK_API_BASE_URL = "https://www.alphavantage.co/"
+        const val STOCK_API_KEY = "LBWSCWL8M5V6V9T0"
 
         const val CRYPTOCURRENCY_API_BASE_URL = "https://www.alphavantage.co/"
     }
