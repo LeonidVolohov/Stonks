a12b8faaf8189fd25e502965e4688e3d76790a62
owlerxan <44863757+owlerxan@users.noreply.github.com>
2021-02-19 23:41:17 +0300

    Added chart plotting for crypto-currency (#37)
    * Added chart plotting for crypto-currency.
    Some standartization changes.
    
    * Minor ui changes
    
    Co-authored-by: Pavel <pahsagess2@mail.ru>

---
+++ app/src/main/java/com/stonks/api/cryptocurrency/CryptoCurrencyApiUtils.kt
@@ -0,0 +1,129 @@
+package com.stonks.api.cryptocurrency
+
+import com.stonks.api.Constants
+import com.stonks.api.cryptoCurrencyApi
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.time.*
+import java.time.format.DateTimeFormatter
+
+class CryptoCurrencyApiUtils (private val cryptoCurrencyName: String) {
+
+    private companion object {
+        const val dateFormatDaily = "yyyy-MM-dd"
+        val endDateTimeDaily: ZonedDateTime
+            get() = ZonedDateTime.of(
+                    LocalDate.now(),
+                    LocalTime.MIDNIGHT,
+                    ZoneId.systemDefault()
+            )
+    }
+
+    //  Properties to cache results
+    private var lastUpdatedDailyData: ZonedDateTime? = null
+    private var dailyData: List<Pair<ZonedDateTime, Double>>? = null
+
+    fun getPricesFor1Week(): Observable<CryptoCurrencyDataModel.RatesProcessed> {
+        val startDateTime: ZonedDateTime =
+                endDateTimeDaily - Period.of(0, 0, 7)
+        return getDailyPrices().map {
+            CryptoCurrencyDataModel.RatesProcessed(
+                    filterPeriod(it, startDateTime, endDateTimeDaily).toMap().toSortedMap()
+            )
+        }
+    }
+
+    fun getPricesFor1Month(): Observable<CryptoCurrencyDataModel.RatesProcessed> {
+        val startDateTime: ZonedDateTime =
+                endDateTimeDaily - Period.of(0, 1, 0)
+        return getDailyPrices().map {
+            CryptoCurrencyDataModel.RatesProcessed(
+                    filterPeriod(it, startDateTime, endDateTimeDaily).toMap().toSortedMap()
+            )
+        }
+    }
+
+    fun getPricesFor6Months(): Observable<CryptoCurrencyDataModel.RatesProcessed> {
+        val startDateTime: ZonedDateTime =
+                endDateTimeDaily - Period.of(0, 6, 0)
+        return getDailyPrices().map {
+            CryptoCurrencyDataModel.RatesProcessed(
+                    filterPeriod(it, startDateTime, endDateTimeDaily).toMap().toSortedMap()
+            )
+        }
+    }
+
+    fun getPricesFor1Year(): Observable<CryptoCurrencyDataModel.RatesProcessed> {
+        val startDateTime: ZonedDateTime =
+                endDateTimeDaily - Period.of(1, 0, 0)
+        return getDailyPrices().map {
+            CryptoCurrencyDataModel.RatesProcessed(
+                    filterPeriod(it, startDateTime, endDateTimeDaily).toMap().toSortedMap()
+            )
+        }
+    }
+
+    fun getPricesFor5Years(): Observable<CryptoCurrencyDataModel.RatesProcessed> {
+        val startDateTime: ZonedDateTime =
+                endDateTimeDaily - Period.of(5, 0, 0)
+        return getDailyPrices().map {
+            CryptoCurrencyDataModel.RatesProcessed(
+                    filterPeriod(it, startDateTime, endDateTimeDaily).toMap().toSortedMap()
+            )
+        }
+    }
+
+    fun getPricesForCustomPeriod(
+            startDateTime: ZonedDateTime,
+            endDateTime: ZonedDateTime
+    ): Observable<CryptoCurrencyDataModel.RatesProcessed> {
+        return getDailyPrices().map {
+            CryptoCurrencyDataModel.RatesProcessed(
+                    filterPeriod(it, startDateTime, endDateTime).toMap().toSortedMap()
+            )
+        }
+    }
+
+    private fun filterPeriod(
+            fullData: List<Pair<ZonedDateTime, Double>>,
+            startDateTime: ZonedDateTime,
+            endDateTime: ZonedDateTime
+    ): List<Pair<ZonedDateTime, Double>> = fullData.filter {
+        it.first >= startDateTime && it.first <= endDateTime
+    }
+
+    /**
+     * Returns data about daily prices as an Observable
+     */
+    private fun getDailyPrices(): Observable<List<Pair<ZonedDateTime, Double>>> {
+        if (checkDailyDataValid()) {
+            return Observable.just(dailyData)
+        } else {
+            return cryptoCurrencyApi
+                    .getDailyStats(cryptoCurrencyName = cryptoCurrencyName, apikey = Constants.CRYPTOCURRENCY_API_KEY)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .map { result ->
+                        dailyData = result.data.map {
+                            ZonedDateTime.of(
+                                    LocalDate.parse(it.key, DateTimeFormatter.ofPattern(dateFormatDaily)),
+                                    LocalTime.MIDNIGHT,
+                                    ZoneId.of(result.metaData.timeZone)
+                            ).withZoneSameInstant(ZoneId.systemDefault()) to it.value.price.toDouble()
+                        }
+                        lastUpdatedDailyData = ZonedDateTime.now(ZoneId.systemDefault())
+                        dailyData
+                    }
+        }
+    }
+
+    /**
+     * Checks whether we can return cached daily data or a new call ro API is needed
+     *
+     * @return Boolean showing whether daily data is still up-to-date'ish
+     */
+    private fun checkDailyDataValid(): Boolean = lastUpdatedDailyData
+            ?.isAfter(ZonedDateTime.now(ZoneId.systemDefault()) - Period.ofDays(1))
+            ?: false
+}
--- app/src/main/java/com/stonks/api/cryptocurrency/CryptoCurrencyRequests.kt
+++ app/src/main/java/com/stonks/api/cryptocurrency/CryptoCurrencyRequests.kt
@@ -1,5 +1,6 @@
 package com.stonks.api.cryptocurrency
 
+import com.stonks.api.Constants
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,8 +8,18 @@ import retrofit2.http.Query
 interface CryptoCurrencyRequests {
 
     @GET("query")
-    fun getCryptoCurrencyRatePerDay(@Query("function") function: String,
-                                    @Query("from_currency") cryptoCurrencyName: String,
-                                    @Query("to_currency") toCurrencyName: String,
-                                    @Query("apikey") apiKey: String): Observable<CryptoCurrencyPerDay>
+    fun getCryptoCurrencyRatePerDay(
+            @Query("function") function: String,
+            @Query("from_currency") cryptoCurrencyName: String,
+            @Query("to_currency") toCurrencyName: String,
+            @Query("apikey") apiKey: String
+    ): Observable<CryptoCurrencyDataModel.CryptoCurrencyPerDay>
+
+    @GET("query")
+    fun getDailyStats (
+            @Query("symbol") cryptoCurrencyName: String,
+            @Query("market") market: String = "USD",
+            @Query("function") function: String = "DIGITAL_CURRENCY_DAILY",
+            @Query("apikey") apikey: String = Constants.CRYPTOCURRENCY_API_KEY
+    ): Observable<CryptoCurrencyDataModel.ResultDaily>
 }
