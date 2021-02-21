3eea0e468c74f3c5c8401c27e22c6c71eb9c0754
kramarov-evg <38880539+kramarov-evg@users.noreply.github.com>
2021-02-21 18:58:42 +0300

    Added basic onboarding (#41)
    * Added basic onboarding:
    - Added preferences setup screen
    - Added saving preferences to SharedPreferences
    - Added semi-transparent guide to app
    - Configured to show guide and pref-s setup only on first launch
    - Some new icons
    - Managed translations and default currency

---
+++ app/src/main/java/com/stonks/OnboardingActivity.kt
@@ -0,0 +1,100 @@
+package com.stonks
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import com.stonks.ui.main.MainActivity
+import java.util.*
+
+class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var btnSavePrefs: Button
+    private lateinit var spinnerDefaultCurrency: Spinner
+    private lateinit var groupLanguageSelection: RadioGroup
+
+    private val sharedPrefs: SharedPreferences
+        get() = getPreferences(Context.MODE_PRIVATE)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        var (firstLaunch, language, defaultCurrencyInd) = retrievePrefs()
+
+        if (firstLaunch) {
+            setContentView(R.layout.activity_onboarding)
+            btnSavePrefs = findViewById(R.id.save_preferences_btn)
+            spinnerDefaultCurrency = findViewById(R.id.default_currency_selector_spinner)
+            groupLanguageSelection = findViewById(R.id.language_selection_group)
+
+            spinnerDefaultCurrency.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        defaultCurrencyInd = position
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {}
+                }
+
+            btnSavePrefs.setOnClickListener {
+                when (groupLanguageSelection.checkedRadioButtonId) {
+                    R.id.english_language -> {
+                        language = "en"
+                    }
+                    R.id.russian_language -> {
+                        language = "ru"
+                    }
+                }
+                with(sharedPrefs.edit()) {
+                    putBoolean("firstLaunch", false)
+                    putInt("defaultCurrencyInd", defaultCurrencyInd)
+                    putString("language", language)
+                    commit()
+                }
+                callMain(language, firstLaunch, defaultCurrencyInd)
+            }
+        } else {
+            callMain(language, firstLaunch, defaultCurrencyInd)
+        }
+    }
+
+    private fun callMain(language: String, firstLaunch: Boolean, defaultCurrencyInd: Int) {
+        updateLocale(language)
+        val intent = Intent(this, MainActivity::class.java)
+        // To pass any data to next activity
+        intent.putExtra("firstLaunch", firstLaunch)
+        intent.putExtra("defaultCurrencyInd", defaultCurrencyInd)
+        // start your next activity
+        startActivity(intent)
+    }
+
+    private fun retrievePrefs(): Triple<Boolean, String, Int> {
+        val firstLaunch = sharedPrefs.getBoolean("firstLaunch", true)
+        val language = sharedPrefs.getString("language", "en").toString()
+        val defaultCurrencyInd = sharedPrefs.getInt("defaultCurrencyInd", 0)
+        return Triple(firstLaunch, language, defaultCurrencyInd)
+
+    }
+
+    private fun updateLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config: Configuration = resources.configuration
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
+    }
+}
--- app/src/main/java/com/stonks/ui/main/MainActivity.kt
+++ app/src/main/java/com/stonks/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.stonks.ui.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -16,20 +18,51 @@ class MainActivity : AppCompatActivity() {
     // Variable storing currently selected fragment for displaying
     private var selectedFragment: Fragment = CurrencyFragment(bottomNavigationHeight)
 
+    private lateinit var rlGuideFirstPage: RelativeLayout
+    private lateinit var rlGuideSecondPage: RelativeLayout
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNavigation.setOnNavigationItemSelectedListener(navListener)
-        bottomNavigation.selectedItemId = R.id.currency_tab
+        val defaultCurrencyInd = intent.getIntExtra("defaultCurrencyInd", 0)
+        val firstLaunch = intent.getBooleanExtra("firstLaunch", true)
 
+        rlGuideSecondPage = findViewById(R.id.guide_overlay_second_layout)
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        rlGuideFirstPage = findViewById(R.id.guide_overlay_first_layout)
+        rlGuideSecondPage.visibility = View.GONE
+
+        if (firstLaunch) {
+            rlGuideFirstPage.setOnClickListener {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commitNow()
+                rlGuideSecondPage.visibility = View.VISIBLE
+                rlGuideFirstPage.visibility = View.GONE
+            }
+            rlGuideSecondPage.setOnClickListener {
+                rlGuideSecondPage.visibility = View.GONE
+                bottomNavigation.menu.getItem(0).isEnabled = true
+                bottomNavigation.menu.getItem(2).isEnabled = true
+            }
+        } else {
+            rlGuideFirstPage.visibility = View.GONE
+        }
+        bottomNavigation.selectedItemId = R.id.currency_tab
         supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, selectedFragment)
-                .commit()
+            .replace(R.id.fragment_container, selectedFragment)
+            .commitNow()
+
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener)
+        if (firstLaunch) {
+            bottomNavigation.menu.getItem(0).isEnabled = false
+            bottomNavigation.menu.getItem(2).isEnabled = false
+        }
     }
 
-    private val navListener : BottomNavigationView.OnNavigationItemSelectedListener =
+    private val navListener: BottomNavigationView.OnNavigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
             selectedFragment = when (item.itemId) {
                 R.id.cryptocurrency_tab -> CryptoFragment()
@@ -39,8 +72,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, selectedFragment)
-                    .commit()
+                .replace(R.id.fragment_container, selectedFragment)
+                .commit()
 
             true
         }
