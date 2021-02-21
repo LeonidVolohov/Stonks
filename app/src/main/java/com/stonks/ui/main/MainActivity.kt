333140c1682e88a21b33f1af118737d5a2cafd53
Kramarov Evgenii <kramarov-evg@mail.ru>
2021-02-21 07:27:14 +0300

    Added basic onboarding: - Added preferences setup screen - Added saving preferences to SharedPreferences - Added semi-transparent guide to app - Configured to show guide and pref-s setup only on first launch - Some new icons

--- app/src/main/java/com/stonks/ui/main/MainActivity.kt
+++ app/src/main/java/com/stonks/ui/main/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.stonks.ui.main
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stonks.R
-import com.stonks.ui.DefaultFragment
+import com.stonks.ui.cryptocurrency.CryptoFragment
 import com.stonks.ui.currency.CurrencyFragment
 import com.stonks.ui.stocks.StocksFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import com.stonks.ui.cryptocurrency.CryptoFragment
 
 class MainActivity : AppCompatActivity() {
     private val bottomNavigationHeight: Int = 0
@@ -17,17 +21,96 @@ class MainActivity : AppCompatActivity() {
     // Variable storing currently selected fragment for displaying
     private var selectedFragment: Fragment = CurrencyFragment(bottomNavigationHeight)
 
+    private lateinit var rlGuideFirstPage: RelativeLayout
+    private lateinit var rlGuideSecondPage: RelativeLayout
+    private lateinit var clSetupPrefs: ConstraintLayout
+    private lateinit var fragmentContainer: FrameLayout
+    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var btnSavePrefs: Button
+    private lateinit var spinnerDefaultCurrency: Spinner
+    private lateinit var groupLanguageSelection: RadioGroup
+//    private var textHelp: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNavigation.setOnNavigationItemSelectedListener(navListener)
-        bottomNavigation.selectedItemId = R.id.currency_tab
+        val sharedPrefs = getPreferences(Context.MODE_PRIVATE)
+        val firstLaunch = sharedPrefs.getBoolean("first_launch", true)
+        Log.i("MAIN", "SharedPrefsValue: $firstLaunch")
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, selectedFragment)
-            .commit()
+        rlGuideFirstPage = findViewById(R.id.guide_overlay_first_layout)
+        rlGuideSecondPage = findViewById(R.id.guide_overlay_second_layout)
+        clSetupPrefs = findViewById(R.id.preferences_setup_layout)
+        fragmentContainer = findViewById(R.id.fragment_container)
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        btnSavePrefs = findViewById(R.id.save_preferences_btn)
+        spinnerDefaultCurrency = findViewById(R.id.default_currency_selector_spinner)
+        groupLanguageSelection = findViewById(R.id.language_selection_group)
+
+        if (firstLaunch) {
+            var currency = ""
+            var language = ""
+            Log.i("MAIN", "Wrote false to SharedPrefs")
+            Log.i(
+                "MAIN",
+                "Curr value in SharedPrefs: ${sharedPrefs.getBoolean("first_launch", true)}"
+            )
+
+            supportActionBar?.hide()
+            bottomNavigation.visibility = View.GONE
+            rlGuideFirstPage.visibility = View.GONE
+            rlGuideSecondPage.visibility = View.GONE
+
+            btnSavePrefs.setOnClickListener {
+                currency = spinnerDefaultCurrency.selectedItem.toString()
+                when (groupLanguageSelection.checkedRadioButtonId) {
+                    R.id.english_language -> {
+                        language = "eng"
+                    }
+                    R.id.russian_language -> {
+                        language = "ru"
+                    }
+                }
+                with(sharedPrefs.edit()) {
+                    putBoolean("first_launch", false)
+                    putString("currency", currency)
+                    putString("language", language)
+                    commit()
+                }
+                clSetupPrefs.visibility = View.GONE
+                bottomNavigation.visibility = View.VISIBLE
+                rlGuideFirstPage.visibility = View.VISIBLE
+            }
+
+            rlGuideFirstPage.setOnClickListener {
+                bottomNavigation.selectedItemId = R.id.currency_tab
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commitNow()
+                rlGuideSecondPage.visibility = View.VISIBLE
+                rlGuideFirstPage.visibility = View.GONE
+            }
+            rlGuideSecondPage.setOnClickListener {
+                rlGuideSecondPage.visibility = View.GONE
+                bottomNavigation.menu.getItem(0).isEnabled = true
+                bottomNavigation.menu.getItem(2).isEnabled = true
+            }
+        } else {
+            rlGuideFirstPage.visibility = View.GONE
+            rlGuideSecondPage.visibility = View.GONE
+            clSetupPrefs.visibility = View.GONE
+            bottomNavigation.selectedItemId = R.id.currency_tab
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, selectedFragment)
+                .commitNow()
+        }
+
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener)
+        if (firstLaunch) {
+            bottomNavigation.menu.getItem(0).isEnabled = false
+            bottomNavigation.menu.getItem(2).isEnabled = false
+        }
     }
 
     private val navListener : BottomNavigationView.OnNavigationItemSelectedListener =
