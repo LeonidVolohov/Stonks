6cc41703b6e8524b0dfa8de8c5bc70da79d17b9f
Evgenii Kramarov <kramarov-evg@mail.ru>
2021-04-05 01:18:12 +0300

    Added endpoint support for UrlBuilder
    Extras: Refactor + doc

---
+++ app/src/main/java/com/stonks/api/utils/UrlBuilder.kt
@@ -0,0 +1,33 @@
+package com.stonks.api.utils
+
+import android.net.Uri
+
+/**
+ * Help class for constructing request URLs
+ */
+object UrlBuilder {
+    /**
+     * Utility function that builds URL based on [baseUrl]
+     *
+     * @param baseUrl The URL used for construction as String (e.g. http://example.com)
+     * @param endpoint (Optional) endpoint as a String.
+     * If provided, specified endpoint is appended to [baseUrl]
+     * @param params (Optional) HashMap of parameters.
+     * If provided, specified parameters are added to resulting URL
+     *
+     * @return Resulting constructed URL as a String
+     */
+    fun build(baseUrl: String, endpoint: String? = null, params: HashMap<String, Any>? = null): String {
+        val builder = Uri.parse(baseUrl)
+            .buildUpon()
+        if (endpoint != null) {
+            builder.appendPath(endpoint)
+        }
+        if (params != null) {
+            for ((param, value) in params.entries) {
+                builder.appendQueryParameter(param, value.toString())
+            }
+        }
+        return builder.build().toString()
+    }
+}
