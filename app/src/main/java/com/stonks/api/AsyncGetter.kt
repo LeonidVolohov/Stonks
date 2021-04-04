a1242de2af18431544abb0a48fb76abc9634c602
Evgenii Kramarov <kramarov-evg@mail.ru>
2021-04-04 16:14:26 +0300

    Created async getting class to migrate requests to

---
+++ app/src/main/java/com/stonks/api/AsyncGetter.kt
@@ -0,0 +1,27 @@
+package com.stonks.api
+
+import android.os.AsyncTask
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class AsyncGetter : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            val okHttpClient = OkHttpClient()
+            if (params.isNotEmpty()) {
+                val request = Request.Builder()
+                    .url(
+                        params[0] ?: ""
+                    )
+                    .build()
+                val response = okHttpClient.newCall(request).execute()
+                Log.e("AAAAAAAAAAAAAA", response.toString())
+                return response.body?.string() ?: ""
+            } else {
+                Log.e(TAG, "No server URL passed. Nothing to fetch")
+                return ""
+            }
+        }
+
+    private val TAG = this::class.java.name
+}
