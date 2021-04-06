7fd32d688f1c6c59d3f18ec99b4952f0e5e05fc5
Evgenii Kramarov <kramarov-evg@mail.ru>
2021-04-06 17:09:59 +0300

    Logging added for requests

--- app/src/main/java/com/stonks/api/AsyncGetter.kt
+++ app/src/main/java/com/stonks/api/AsyncGetter.kt
@@ -4,10 +4,16 @@ import android.os.AsyncTask
 import android.util.Log
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
 
 class AsyncGetter : AsyncTask<String, Void, String>() {
     override fun doInBackground(vararg params: String?): String {
-        val okHttpClient = OkHttpClient()
+        val interceptor: HttpLoggingInterceptor =
+            HttpLoggingInterceptor().apply { this.level = HttpLoggingInterceptor.Level.BODY }
+
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
         if (params.isNotEmpty()) {
             val request = Request.Builder()
                 .url(
